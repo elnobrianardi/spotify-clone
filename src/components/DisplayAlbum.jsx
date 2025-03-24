17d606d35d7c9738731667178264b883c1947a92
elnobrianardi <elno.brianardi@gmail.com>
2025-03-24 13:59:53 +0700

    chore: add checkbox

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -14,11 +14,11 @@ const DisplayAlbum = () => {
   return (
     <>
       <Navbar />
-      <div className="mt-10 flex gap-8 flex-col md:flex-row md:items-end">
+      <div className="flex flex-col gap-8 mt-10 md:flex-row md:items-end">
         <img className="w-48 rounded" src={albumData.image} alt="" />
         <div className="flex flex-col">
           <p>Playlist</p>
-          <h2 className="text-5xl font-bold mb-4 md:text-7xl">
+          <h2 className="mb-4 text-5xl font-bold md:text-7xl">
             {albumData.name}
           </h2>
           <h4>{albumData.desc}</h4>
@@ -38,15 +38,16 @@ const DisplayAlbum = () => {
         </p>
         <p>Album</p>
         <p className="hidden sm:block">Date Added</p>
-        <img className="m-auto w-4" src={assets.clock_icon} alt="" />
+        <img className="w-4 m-auto" src={assets.clock_icon} alt="" />
       </div>
       <hr />
       {
         songsData.map((item, index) => (
           <div onClick={() => playWithId(item.id)} key={index} className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer">
             <p className="text-white text-[15px] line-clamp-1">
+                <input type='checkbox'></input>
                 <b className="mr-4 text-[#a7a7a7]">{index + 1}.</b>
-                <img className="inline w-7 mr-5 md:w-10" src={item.image} alt="" />
+                <img className="inline mr-5 w-7 md:w-10" src={item.image} alt="" />
                 {item.name}
             </p>
             <p className="text-[15px] ">{albumData.name}</p>
